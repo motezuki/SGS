9bbe91cf07ef79a90218f4f31ae5b5ed40bd973d
Fabio Motezuki <95715242+motezuki@users.noreply.github.com>
2025-09-18 23:09:03 -0300

    Clean up lambda_handler by removing comments
    Removed commented-out logging code from lambda_handler.

--- SGS/handler.py
+++ SGS/handler.py
@@ -1,10 +1,6 @@
-# import json
 import serverless_wsgi
 from app import app
 
 def lambda_handler(event, context):
-    # Log the incoming event
-    # print("Received event:", json.dumps(event))
-    # print("Received context:", json.dumps(context))
 
     return serverless_wsgi.handle_request(app.server, event, context)
