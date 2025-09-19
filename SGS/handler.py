4b89c7f5cfa0e15176b251b0136815a5659c9e49
Fabio Motezuki <95715242+motezuki@users.noreply.github.com>
2025-09-19 01:22:25 +0000

    ajusta PYTHONPATH no linting da função Lambda e remove importações desnecessárias em handler.py

--- SGS/handler.py
+++ SGS/handler.py
@@ -1,10 +1,6 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
 # import json
 import serverless_wsgi
-from SGS.app import app
+from app import app
 
 def lambda_handler(event, context):
     # Log the incoming event
